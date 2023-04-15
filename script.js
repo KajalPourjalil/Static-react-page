c5f6999c394d9ea95a460990031354e1667f35b1
Kajal Pourjalil <karastar771@gmail.com>
2023-04-15 18:29:27 +0300

    practice

--- script.js
+++ script.js
@@ -11,16 +11,42 @@ function MainContent() {
     )
 }
 
-const h1 = document.createElement("h1")
-h1.textContent = "Imperative way"
-h1.className = "header"
-document.getElementsById("root").append(h1)
+// const h1 = document.createElement("h1")
+// h1.textContent = "Imperative way"
+// h1.className = "header"
+// document.getElementsById("root").append(h1)
 
 function New() {
     return(
         <h1>Ela e em minha destinada!</h1>
     )
 }
+
+function Navbar() {
+    return (
+        <nav>
+            <h1>Practice React: project #1</h1>
+            <ul>
+                <li>Pricing</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+}
+
+const navbar = (
+    <nav>
+        <h1>Practice React: project #1</h1>
+            <ul>
+                <li>Pricing</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+    </nav>
+)
+
+ReactDOM.render(navbar, document.getElementById("now"))
 
 
 
@@ -31,6 +57,7 @@ ReactDOM.render(
   <Hello/>
   <MainContent/>
   <New/>
+  {/* <Navbar/> */}
     
   </div>, document.getElementById("root")
 );
