0a946fe983100929eced7c4d917b1547382fc88d
Kajal Pourjalil <karastar771@gmail.com>
2023-04-17 13:18:10 +0300

    Static page

--- script.js
+++ script.js
@@ -1,63 +1,104 @@
+// function Hello () {
+//     return(
+//         <h1>Ola Alexandra</h1>
+//     )
+// }
 
-function Hello () {
-    return(
-        <h1>Ola Alexandra</h1>
-    )
-}
+// function MainContent() {
+//     return(
+//         <h1>I'm learning React</h1>
+//     )
+// }
 
-function MainContent() {
-    return(
-        <h1>I'm learning React</h1>
-    )
-}
+// // const h1 = document.createElement("h1")
+// // h1.textContent = "Imperative way"
+// // h1.className = "header"
+// // document.getElementsById("root").append(h1)
 
-// const h1 = document.createElement("h1")
-// h1.textContent = "Imperative way"
-// h1.className = "header"
-// document.getElementsById("root").append(h1)
+// function New() {
+//     return(
+//         <h1>Ela e em minha destinada!</h1>
+//     )
+// }
 
-function New() {
-    return(
-        <h1>Ela e em minha destinada!</h1>
-    )
-}
+// function Navbar() {
+//     return (
+//         <nav>
+//             <h1>Practice React: project #1</h1>
+//             <ul>
+//                 <li>Pricing</li>
+//                 <li>About</li>
+//                 <li>Contact</li>
+//             </ul>
+//         </nav>
+//     )
+// }
 
-function Navbar() {
-    return (
-        <nav>
-            <h1>Practice React: project #1</h1>
-            <ul>
-                <li>Pricing</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
-        </nav>
-    )
-}
+// const navbar = (
+//     <nav>
+//         <h1>Practice React: project #1</h1>
+//             <ul>
+//                 <li>Pricing</li>
+//                 <li>About</li>
+//                 <li>Contact</li>
+//             </ul>
+//     </nav>
+// )
 
-const navbar = (
-    <nav>
-        <h1>Practice React: project #1</h1>
-            <ul>
-                <li>Pricing</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
-    </nav>
+// ReactDOM.render(navbar, document.getElementById("now"))
+
+// const Element = (
+//     <div>
+//         <h2>Eu sou digno dela!</h2>
+//         <p>I am doing this for her and for me</p>
+//         <p>Ela e: </p>
+//         <ul>
+//             <li>Gostosa</li>
+//             <li>Querida</li>
+//             <li>Corajosa</li>
+//             <li>Elegente</li>
+//             <li>Incrivel</li>
+//         </ul>
+//     </div>
+// )
+
+
+
+
+// ReactDOM.render(Element, document.getElementById('now'))
+// document.getElementById('now').append(Element)
+
+
+// ReactDOM.render(
+//   <div>
+//   <Hello/>
+//   <MainContent/>
+//   <New/>
+//   <Navbar/>
+    
+//   </div>, document.getElementById("root")
+// );
+
+
+// ReactDOM.createRoot(document.getElementById("root")).render(navbar)
+
+
+const page = (
+    <div>
+        <img src="reactjs_logo_icon_170805.png" width="200px"/>
+        <h1>Fun facts about React</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on Github</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </div>
 )
 
-ReactDOM.render(navbar, document.getElementById("now"))
+
+ReactDOM.render(page, document.getElementById('root'))
 
 
 
-
-
-ReactDOM.render(
-  <div>
-  <Hello/>
-  <MainContent/>
-  <New/>
-  {/* <Navbar/> */}
-    
-  </div>, document.getElementById("root")
-);
